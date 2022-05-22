a5b629199bbc47a427347d9553917906e418e6b1
Sergei <inkognitoserjo@gmail.com>
2022-05-22 21:19:50 +0300

    add new

---
+++ src/main/java/h_z_3.java
@@ -0,0 +1,8 @@
+public class h_z_3 {
+    public static void main (String[] agrs) {
+    String[] word = {"H", "e", "l", "l", "o"};
+    for (int i = word.length-1; i>=0; i--)
+        System.out.println(word[i]);
+
+    }
+}
