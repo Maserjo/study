e24469d33442de9e61c690f7b478a0c7d0e484b6
Sergei <inkognitoserjo@gmail.com>
2022-05-22 17:58:49 +0300

    Initial commit

---
+++ src/main/java/Study.java
@@ -0,0 +1,9 @@
+public class Study {
+
+    public static void main(String[] agrs) {
+        String word = "home";
+        String newWord =
+                word.substring(0, 1).toUpperCase() + word.substring(1, word.length() - 1) + word.substring(word.length() - 1).toUpperCase();
+        System.out.println(newWord);
+    }
+}
