3b967bacaa9f9e64fe3efb32bd9751f8f1d038aa
Sergei <inkognitoserjo@gmail.com>
2022-05-22 21:30:21 +0300

    add new

---
+++ src/main/java/h_z_4.java
@@ -0,0 +1,10 @@
+public class h_z_4 {
+    public static void main (String[] agrs){
+     int[] num = {1, 2, 3, 4, 5};
+     int sum = 0;
+     for (int sum1 : num)
+         sum = sum + sum1;
+     int averege = sum / num.length;
+     System.out.println(averege);
+    }
+}
