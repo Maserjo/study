ba874bcfec77caca6729064708fffa19579b0d21
Sergei <inkognitoserjo@gmail.com>
2022-05-25 19:47:56 +0300

    add new

--- src/main/java/may_25_1/CL1.java
+++ src/main/java/may_25_1/CL1.java
@@ -1,4 +1,21 @@
 package may_25_1;
 
 public class CL1 {
+    public static void main (String[] agrs){
+        System.out.println(maxvalue(12, 14, 1));
+
+    }
+    public static int maxvalue (int a, int b, int c){
+        int max;
+        if (a > b && a > c)
+            max = a;
+        else
+            if (b > a && b > c)
+                max = b;
+        else max = c;
+        return max;
+
+
+    }
+
 }
